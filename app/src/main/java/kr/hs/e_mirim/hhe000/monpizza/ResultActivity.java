2d3a0a3682cffc61d5516cfbc27133efc4d3d511
hyeongeun Ham <hheooo@naver.com>
2016-03-28 20:53:18 +0900

    #4 Implement TextView Result Activity

--- app/src/main/java/kr/hs/e_mirim/hhe000/monpizza/ResultActivity.java
+++ app/src/main/java/kr/hs/e_mirim/hhe000/monpizza/ResultActivity.java
@@ -1,17 +1,30 @@
 package kr.hs.e_mirim.hhe000.monpizza;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
  * Created by appcreator20 on 2016. 3. 28..
  */
 public class ResultActivity extends AppCompatActivity {
+    TextView mResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mResult = (TextView) findViewById(R.id.result);
+        mResult.setText("결과");
+
+
     }
+
 }
