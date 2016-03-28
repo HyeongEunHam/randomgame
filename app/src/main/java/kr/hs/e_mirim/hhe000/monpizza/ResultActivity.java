14ecdfaf020a11d60d6b2f4babc07952da1ec49b
hyeongeun Ham <hheooo@naver.com>
2016-03-28 20:14:02 +0900

    #2 add Result Activity

---
+++ app/src/main/java/kr/hs/e_mirim/hhe000/monpizza/ResultActivity.java
@@ -0,0 +1,17 @@
+package kr.hs.e_mirim.hhe000.monpizza;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by appcreator20 on 2016. 3. 28..
+ */
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
